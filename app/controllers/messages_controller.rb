4e7b5b0a59ebc7977173d5b749faf9aa6d4790b7
sun <jelani@firstdraft.com>
2019-03-05 21:58:55 -0600

    Fix message partial and chat form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,9 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    message = Message.new(params[:message].permit!)
+    message.user = current_user
+    message.save
   end
 end
