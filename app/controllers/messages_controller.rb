ef933bd9561a2aa8f7d695f0e2066cf5b0e80406
sun <jelani@firstdraft.com>
2019-03-05 22:03:15 -0600

    Got a basic AJAX refresh on form submission

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,8 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    message = Message.new(params[:message].permit!)
-    message.user = current_user
-    message.save
+    @message = Message.new(params[:message].permit!)
+    @message.user = current_user
+    @message.save
   end
 end
